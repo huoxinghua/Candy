436f0edb29a85d37a5534fd3d60ccef1d2253421
sayed akid <samsayed07866@gmail.com>
2024-10-31 14:35:37 -0600

    collectable sound

--- Assets/Xinghua/XinghuaScript/Inventory/ItemCollect.cs
+++ Assets/Xinghua/XinghuaScript/Inventory/ItemCollect.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,13 +8,29 @@ public class ItemCollect : MonoBehaviour
 {
     public UnityEvent CameraTrigger;
     protected string  itemName;
-   
+    public AudioClip collectedSound;
+    private AudioSource audioSource;
+
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null )
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerController>())
         {
             gameObject.SetActive(false);
             //add collect sound here
+            if (collectedSound != null)
+            {
+                audioSource.PlayOneShot(collectedSound);
+            }
            
            if (GameManager.Instance != null)
             {
@@ -25,6 +42,7 @@ public class ItemCollect : MonoBehaviour
             }
            // CameraTrigger?.Invoke();
         }
+
     }
 
 }
