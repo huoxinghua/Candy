12bbc90fb8763961c1c7ca2d79ce1a562e8b4a4e
sayed akid <samsayed07866@gmail.com>
2024-10-31 15:49:25 -0600

    collect sound and loose menu changes

--- Assets/Xinghua/XinghuaScript/Inventory/ItemCollect.cs
+++ Assets/Xinghua/XinghuaScript/Inventory/ItemCollect.cs
@@ -8,29 +8,16 @@ public class ItemCollect : MonoBehaviour
 {
     public UnityEvent CameraTrigger;
     protected string  itemName;
-    public AudioClip collectedSound;
-    private AudioSource audioSource;
+    
 
-
-    private void Awake()
-    {
-        audioSource = GetComponent<AudioSource>();
-        if (audioSource == null )
-        {
-            audioSource = gameObject.AddComponent<AudioSource>();
-        }
-    }
-
+   
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerController>())
         {
             gameObject.SetActive(false);
             //add collect sound here
-            if (collectedSound != null)
-            {
-                audioSource.PlayOneShot(collectedSound);
-            }
+            SoundManager.Instance.PlaySFX("collectSound");
            
            if (GameManager.Instance != null)
             {
